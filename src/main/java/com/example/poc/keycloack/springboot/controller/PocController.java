3703073a3fe7844ff7e0b59b2e6ee2c842747bdb
wssh <wesley.hinsch@matera.com>
2021-02-04 21:58:56 -0300

    primeiro commit

---
+++ src/main/java/com/example/poc/keycloack/springboot/controller/PocController.java
@@ -0,0 +1,37 @@
+package com.example.poc.keycloack.springboot.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+
+@RestController
+@RequestMapping("/test")
+public class PocController {
+
+    @RequestMapping(value = "/anonymous", method = RequestMethod.GET)
+    public ResponseEntity<String> getAnonymous() {
+        return ResponseEntity.ok("Hello Anonymous");
+    }
+
+    @RolesAllowed("user")
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ResponseEntity<String> getUser() {
+        return ResponseEntity.ok("Hello User");
+    }
+
+    @RolesAllowed("admin")
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public ResponseEntity<String> getAdmin() {
+        return ResponseEntity.ok("Hello Admin");
+    }
+
+    @RolesAllowed({ "admin", "user" })
+    @RequestMapping(value = "/all-user", method = RequestMethod.GET)
+    public ResponseEntity<String> getAllUser() {
+        return ResponseEntity.ok("Hello All User");
+    }
+
+}
---
+++ src/test/java/com/example/poc/keycloack/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poc.keycloack.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
